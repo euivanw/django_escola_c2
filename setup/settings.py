994dd936f62e8b6851f3a9003a21f9661416116d
Ivan Wilhelm <contato@ivanwilhelm.dev>
2024-05-14 14:13:31 -0300

    chore: Inclusão da configuração que permite a origem localhost 3000

--- setup/settings.py
+++ setup/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'escola',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,3 +135,7 @@ REST_FRAMEWORK = {
         'user': '1000/day',
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+]
